157d2f2e1f2e6ae818bf72d25c407ebf49a43754
jesseds441 <72114495+jesseds441@users.noreply.github.com>
2022-02-23 20:35:48 -0800

    Update my_game.js

--- src/my_game/my_game.js
+++ src/my_game/my_game.js
@@ -1,37 +1,63 @@
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
+import DyePack from "./dye_pack.js";
+import Hero from "./hero.js";
 import engine from "../engine/index.js";
+import TextureRenderable from "../engine/renderables/texture_renderable_main.js";
+import SpriteRenderable from "../engine/renderables/sprite_renderable.js";
 
 class MyGame extends engine.Scene {
     constructor() {
         super();
 
+        //assets 
+        this.kDyePackSprite = "assets/f_walk.png";
+        this.kBg = "assets/bg.png";
+
         // The camera to view the scene
         this.mCamera = null;
 
+        this.mDyePacks = null;
+
+        this.mHero = null;
+        this.mBg = null;
+
         this.mMsg = null;
     
-        this.mLineSet = [];
-        this.mCurrentLine = null;
-        this.mP1 = null;
+    }
 
-        this.mShowLine = true;
+    load() {
+        engine.texture.load(this.kDyePackSprite);
+        engine.texture.load(this.kBg);
+        
+    }
+
+    unload() {
+        engine.texture.unload(this.kDyePackSprite);
+        engine.texture.unload(this.kBg);
     }
         
     init() {
         // Step A: set up the cameras
         this.mCamera = new engine.Camera(
             vec2.fromValues(30, 27.5), // position of the camera
-            100,                       // width of camera
-            [0, 0, 640, 480]           // viewport (orgX, orgY, width, height)
+            200,                       // width of camera
+            [0, 0, 800, 600]           // viewport (orgX, orgY, width, height)
         );
         this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
                 // sets the background to gray
     
+
         this.mMsg = new engine.FontRenderable("Status Message");
-        this.mMsg.setColor([0, 0, 0, 1]);
-        this.mMsg.getXform().setPosition(-19, -8);
-        this.mMsg.setTextHeight(3);
+        this.mMsg.setColor([1, 1, 1, 1]);
+        this.mMsg.getXform().setPosition(-65, -40);
+        this.mMsg.setTextHeight(3); 
+
+        this.mBg = new engine.TextureRenderable(this.kBg);
+        this.mBg.getXform().setSize(400, 400);
+        this.mBg.getXform().setPosition(50, 40);
+
+        this.mDyePacks = new engine.GameObjectSet()
     }
     
     // This is the draw function, make sure to setup proper drawing environment, and more
@@ -41,68 +67,37 @@ class MyGame extends engine.Scene {
         engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
     
         this.mCamera.setViewAndCameraMatrix();
-        let i, l;
-        for (i = 0; i < this.mLineSet.length; i++) {
-            l = this.mLineSet[i];
-            l.draw(this.mCamera);
-        }
+        this.mBg.draw(this.mCamera); 
+        this.mDyePacks.draw(this.mCamera);
+        
         this.mMsg.draw(this.mCamera);   // only draw status in the main camera
     }
     
     // The Update function, updates the application state. Make sure to _NOT_ draw
     // anything from this function!
     update () {
-        let msg = "Lines: " + this.mLineSet.length + " ";
-        let echo = "";
-        let x, y;
-        
-        // show line or point
-        if  (engine.input.isKeyClicked(engine.input.keys.P)) {
-            this.mShowLine = !this.mShowLine;
-            let line = null;
-            if (this.mCurrentLine !== null)
-                line = this.mCurrentLine;
-            else {
-                if (this.mLineSet.length > 0)
-                    line = this.mLineSet[this.mLineSet.length-1];
-            }
-            if (line !== null)
-                line.setShowLine(this.mShowLine);
+        let msg = "Status: DyePacks(" + this.mDyePacks.size() + ") " + "Patrols() Autospawn()";
+  
+        // Get DyePack to spawn
+        if (engine.input.isKeyClicked(engine.input.keys.Space)) {
+
+            let dyePack = new DyePack(this.kDyePackSprite, 
+                this.mCamera.mouseWCX(), this.mCamera.mouseWCY());
+            
+            //add to gameobject set
+            this.mDyePacks.addToSet(dyePack);
+
+            console.log(this.mDyePacks);
         }
-    
-        if (engine.input.isButtonPressed(engine.input.eMouseButton.eMiddle)) {
-            let len = this.mLineSet.length;
-            if (len > 0) {
-                this.mCurrentLine = this.mLineSet[len - 1];
-                x = this.mCamera.mouseWCX();
-                y = this.mCamera.mouseWCY();
-                echo += "Selected " + len + " ";
-                echo += "[" + x.toPrecision(2) + " " + y.toPrecision(2) + "]";
-                this.mCurrentLine.setFirstVertex(x, y);
+
+        if (this.mDyePacks.size() > 0) {
+            // if DyePack should be deleted
+            if (this.mDyePacks.getObjectAt(0).getToDelete()) { 
+                this.mDyePacks.removeFirstFromSet();
             }
         }
-    
-        if (engine.input.isButtonPressed(engine.input.eMouseButton.eLeft)) {
-            x = this.mCamera.mouseWCX();
-            y = this.mCamera.mouseWCY();
-            echo += "[" + x.toPrecision(2) + " " + y.toPrecision(2) + "]";
-    
-            if (this.mCurrentLine === null) { // start a new one
-                this.mCurrentLine = new engine.LineRenderable();
-                this.mCurrentLine.setFirstVertex(x, y);
-                this.mCurrentLine.setPointSize(5.0);
-                this.mCurrentLine.setShowLine(this.mShowLine);
-                this.mLineSet.push(this.mCurrentLine);
-            } else {
-                this.mCurrentLine.setSecondVertex(x, y);
-            }
-        } else {
-            this.mCurrentLine = null;
-            this.mP1 = null;
-        }
-    
-        msg += echo;
-        msg += " Show:" + (this.mShowLine ? "Ln" : "Pt");
+
+        this.mDyePacks.update();
         this.mMsg.setText(msg);
     }
 }
